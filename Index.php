cfa64e042f42bc9ad0044ba54ba06cf42794e568
gnu-xiaosong <70237653+gnu-xiaosong@users.noreply.github.com>
2020-10-09 17:25:06 +0800

    添加了时间查询分装类库文件

--- Index.php
+++ Index.php
@@ -1,28 +1,11 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
-use think\helper;
 class Index extends Controller
 {
     //接口api1
-    public function api($type)   
+    public function api($qu)   
     {
-    
-    //请求url
-   // http://ip/index.php/api/$type?dataType=请求数据类型&name=搜索内容
-   
-    //判断请求类型
-    if(strtolower($type)=="get"){
-    //请求为get请求
-    $dataType=input("get.dataType");
-    $name=input("get.name");
-    }else if(strtolower($type)=="post"){
-    //请求为post请求
-    $dataType=input("post.dataType");
-    $name=input("post.name");
-    }
-    //接收数据
-    
     $table="bank3";
     $column="question";
 /************************相关状态值提示:****************************************************
@@ -68,13 +51,13 @@ class Index extends Controller
     
     "arrStatu"=>array(
          "status"=>0,                 //一级状态参数
-         "statu2"=>4                 //二级状态参数
+         "statu2"=>0                 //二级状态参数
      ),
      
     "necessary"=>array(
         "table"=>$table,              //[查询]表名
         "column"=>$column,         //必要字段    $column
-        "name"=>isset($name)?$name:"中国"                //搜索值$name
+        "name"=>$qu                //搜索值$name
     ),
     
     "where"=>array(                  //查询条件数组参数
@@ -105,15 +88,7 @@ class Index extends Controller
     //实例化数据库搜索类
     $mysql=new DbSearch();     
     $data=$mysql->search($arr);     
-    
-   //判断输出的数据类型
-   if(strtolower($dataType)=="array"){
-   //请求数据类型为array时
-     dump($data);
-   }else{
-   //请求数据为json时
-  echo $this->translate($data);
-   }
+    echo $this->translate($data);
    
     }
     
@@ -135,9 +110,62 @@ class Index extends Controller
     return json_decode($data,$assoc);
     }
     }
+  
+    
+    //时间查询测试
+    public function api1(){
+   /* 'today',               //今天
+     'yesterday',           //昨天
+     'week',                //本周
+     'last week',           //上周
+     'month',               //本月
+     'last month',          //上月
+     'year',                 //今年
+     'last year',            //去年
+     '-'.$hours.' '.'hours'    //$hours小时内 int类型
+     */
+    //参数数组封装
+     $arr=array(
+     //necessary options(必须参数配置)
+     "necessary"=>array(
+         "status"=>2,           //状态控制值参数  int类型  默认0
+         "table"=>"xs_bank3",  //表名(全名)    varchar类型  默认
+         "time_column"=>"update_time", //时间字段名
+     ),
+     
+     //able options(可选参数配置)
+     "options"=>array(
+         "time"=>"2020-10-04",    //时间变量
+         "hours"=>2,             //小时变量 int类型  常用于查询几小时内的数据 默认2小时内
+         "item"=>8            //数组下标控制参数 int类型 默认0
+     ),
+     
+     //time interval options(时间区间选项)
+     "interval"=>array(
+     "headTime"=>"2020-04-23",   //start time
+     "footTime"=>"2020-10-04"     //end time
+     ),
+     
+     //configure(配置参数)
+     "configure"=>array(
+        "getMakeSql"=>false,          //是否生成sql语句 boolean类型
+        "ifCache"=> false,              //是否开启缓存查询 boolean累类型
+        "getCacheTime"=>6,            //开启缓存查询的时间 int类型 单位秒
+        "limit"=>100,                      //查询数目限制 不开启 int类型
+        "getOrderColumn"=>"ID",        //排序字段 $orderColoumn 默认id字段    $column=>$name
+        "getOrderRule"=> "asc"        //排序规则$orderRule
+      )
+     );
+     
+     //实例化一个TimeSearch类
+    $mysql=new TimeSearch();
+    //调用方法
+    $data=$mysql->timeSearch($arr);
+    //返回数据类型数组转化为json类型
+    $is_type=false;
+    echo $is_type?$this->translate($data):dump($data);
     
     
-    
-    
-
+    }
+ 
 }
